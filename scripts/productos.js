8699155af28b161aa219541a6a5cfbba69e8cbf3
Thomas DG Jaimes <thomydgj@gmail.com>
2025-10-01 21:01:34 -0500

    prueba de rutas 2

--- scripts/productos.js
+++ scripts/productos.js
@@ -20,11 +20,11 @@ const productos = [
         descripcionLarga: "El empaque Multiflex está diseñado para adaptarse a diferentes tipos de productos, proporcionando durabilidad y flexibilidad.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Multiflex/Multiflex1.png",
-            "../assets/images/productos/Multiflex/Multiflex2.png",
-            "../assets/images/productos/Multiflex/Multiflex3.png",
-            "../assets/images/productos/Multiflex/Multiflex4.png",
-            "../assets/images/productos/Multiflex/Multiflex5.png"
+            "assets/images/productos/Multiflex/Multiflex1.png",
+            "assets/images/productos/Multiflex/Multiflex2.png",
+            "assets/images/productos/Multiflex/Multiflex3.png",
+            "assets/images/productos/Multiflex/Multiflex4.png",
+            "assets/images/productos/Multiflex/Multiflex5.png"
         ],
         categorias: ["carnicos"]
     },
@@ -35,12 +35,12 @@ const productos = [
         descripcionLarga: "El empaque Termoflex ofrece una excelente protección térmica, ideal para productos que requieren mantener su temperatura.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Termoflex/Termoflex1.png",
-            "../assets/images/productos/Termoflex/Termoflex2.png",
-            "../assets/images/productos/Termoflex/Termoflex3.png",
-            "../assets/images/productos/Termoflex/Termoflex4.png",
-            "../assets/images/productos/Termoflex/Termoflex5.png",
-            "../assets/images/productos/Termoflex/Termoflex6.png"
+            "assets/images/productos/Termoflex/Termoflex1.png",
+            "assets/images/productos/Termoflex/Termoflex2.png",
+            "assets/images/productos/Termoflex/Termoflex3.png",
+            "assets/images/productos/Termoflex/Termoflex4.png",
+            "assets/images/productos/Termoflex/Termoflex5.png",
+            "assets/images/productos/Termoflex/Termoflex6.png"
         ],
         categorias: ["carnicos"]
     },
@@ -51,9 +51,9 @@ const productos = [
         descripcionLarga: "La tripa natural de cerdo es perfecta para embutidos y productos cárnicos, ofreciendo una presentación auténtica y tradicional.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Tripa_Natural_Cerdo/Tripa1.png",
-            "../assets/images/productos/Tripa_Natural_Cerdo/Tripa2.png",
-            "../assets/images/productos/Tripa_Natural_Cerdo/Tripa3.png",
+            "assets/images/productos/Tripa_Natural_Cerdo/Tripa1.png",
+            "assets/images/productos/Tripa_Natural_Cerdo/Tripa2.png",
+            "assets/images/productos/Tripa_Natural_Cerdo/Tripa3.png",
         ],
         categorias: ["carnicos"]        
     },
@@ -64,11 +64,11 @@ const productos = [
         descripcionLarga: "El empaque Fibrosa está diseñado para productos cárnicos, proporcionando resistencia y durabilidad durante el almacenamiento y transporte.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Fibrosa/Fibrosa1.png",
-            "../assets/images/productos/Fibrosa/Fibrosa2.png",
-            "../assets/images/productos/Fibrosa/Fibrosa3.png",
-            "../assets/images/productos/Fibrosa/Fibrosa4.png",
-            "../assets/images/productos/Fibrosa/Fibrosa5.png"
+            "assets/images/productos/Fibrosa/Fibrosa1.png",
+            "assets/images/productos/Fibrosa/Fibrosa2.png",
+            "assets/images/productos/Fibrosa/Fibrosa3.png",
+            "assets/images/productos/Fibrosa/Fibrosa4.png",
+            "assets/images/productos/Fibrosa/Fibrosa5.png"
         ],
         categorias: ["carnicos"]
     },
@@ -79,11 +79,11 @@ const productos = [
         descripcionLarga: "El empaque de celulosa es una opción ecológica, biodegradable y sostenible, ideal para productos que buscan reducir su impacto ambiental.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Celulosa/Celulosa1.png",
-            "../assets/images/productos/Celulosa/Celulosa2.png",
-            "../assets/images/productos/Celulosa/Celulosa3.png",
-            "../assets/images/productos/Celulosa/Celulosa4.png",
-            "../assets/images/productos/Celulosa/Celulosa5.png"
+            "assets/images/productos/Celulosa/Celulosa1.png",
+            "assets/images/productos/Celulosa/Celulosa2.png",
+            "assets/images/productos/Celulosa/Celulosa3.png",
+            "assets/images/productos/Celulosa/Celulosa4.png",
+            "assets/images/productos/Celulosa/Celulosa5.png"
         ],
         categorias: ["carnicos"]
     },
@@ -94,8 +94,8 @@ const productos = [
         descripcionLarga: "El Flow-Pack Selle Ventral ofrece un sellado seguro que mantiene la frescura de los productos, ideal para alimentos y snacks.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Flow_Pack_Selle_Ventral/FPSelleVentral1.png",
-            "../assets/images/productos/Flow_Pack_Selle_Ventral/FPSelleVentral2.png",
+            "assets/images/productos/Flow_Pack_Selle_Ventral/FPSelleVentral1.png",
+            "assets/images/productos/Flow_Pack_Selle_Ventral/FPSelleVentral2.png",
 
         ],
         categorias: ["cafe-chocolate", "panaderia"]
@@ -107,10 +107,10 @@ const productos = [
         descripcionLarga: "Este empaque cuenta con un zipper que permite un cierre hermético, ideal para conservar la frescura de los productos y facilitar su almacenamiento.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Bolsa_Flex_Up_Con_Zipper/Zipper1.png",
-            "../assets/images/productos/Bolsa_Flex_Up_Con_Zipper/Zipper2.png",
-            "../assets/images/productos/Bolsa_Flex_Up_Con_Zipper/Zipper3.png",
-            "../assets/images/productos/Bolsa_Flex_Up_Con_Zipper/Zipper4.png"
+            "assets/images/productos/Bolsa_Flex_Up_Con_Zipper/Zipper1.png",
+            "assets/images/productos/Bolsa_Flex_Up_Con_Zipper/Zipper2.png",
+            "assets/images/productos/Bolsa_Flex_Up_Con_Zipper/Zipper3.png",
+            "assets/images/productos/Bolsa_Flex_Up_Con_Zipper/Zipper4.png"
         ],
         categorias: ["cafe-chocolate"]
     },
@@ -121,8 +121,8 @@ const productos = [
         descripcionLarga: "La funda plástica Multiflex es ideal para una variedad de productos, ofreciendo resistencia y versatilidad en su uso.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Funda_Plastica_Multiflex/Multiflex1.png",
-            "../assets/images/productos/Funda_Plastica_Multiflex/Multiflex2.png",
+            "assets/images/productos/Funda_Plastica_Multiflex/Multiflex1.png",
+            "assets/images/productos/Funda_Plastica_Multiflex/Multiflex2.png",
         ],
         categorias: ["mascotas"]
     },
@@ -133,10 +133,10 @@ const productos = [
         descripcionLarga: "Este empaque Flex-Up con Forma está diseñado para adaptarse a la forma del producto, ofreciendo una presentación atractiva y funcional.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Flex_Up_con_Forma/Forma1.png",
-            "../assets/images/productos/Flex_Up_con_Forma/Forma2.png",
-            "../assets/images/productos/Flex_Up_con_Forma/Forma3.png",
-            "../assets/images/productos/Flex_Up_con_Forma/Forma4.png",
+            "assets/images/productos/Flex_Up_con_Forma/Forma1.png",
+            "assets/images/productos/Flex_Up_con_Forma/Forma2.png",
+            "assets/images/productos/Flex_Up_con_Forma/Forma3.png",
+            "assets/images/productos/Flex_Up_con_Forma/Forma4.png",
         ],
         categorias: ["mascotas", "panaderia", "snacks"]
     },
@@ -147,14 +147,14 @@ const productos = [
         descripcionLarga: "El empaque termoformado ofrece una protección superior para productos delicados, manteniéndolos frescos y seguros durante su transporte y almacenamiento.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Termoformados/Termoformados1.png",
-            "../assets/images/productos/Termoformados/Termoformados2.png",
-            "../assets/images/productos/Termoformados/Termoformados3.png",
-            "../assets/images/productos/Termoformados/Termoformados4.png",
-            "../assets/images/productos/Termoformados/Termoformados5.png",
-            "../assets/images/productos/Termoformados/Termoformados6.png",
-            "../assets/images/productos/Termoformados/Termoformados7.png",
-            "../assets/images/productos/Termoformados/Termoformados8.png"
+            "assets/images/productos/Termoformados/Termoformados1.png",
+            "assets/images/productos/Termoformados/Termoformados2.png",
+            "assets/images/productos/Termoformados/Termoformados3.png",
+            "assets/images/productos/Termoformados/Termoformados4.png",
+            "assets/images/productos/Termoformados/Termoformados5.png",
+            "assets/images/productos/Termoformados/Termoformados6.png",
+            "assets/images/productos/Termoformados/Termoformados7.png",
+            "assets/images/productos/Termoformados/Termoformados8.png"
         ],
         categorias: ["alimentos", "cafe-chocolate", "panaderia"]
     },
@@ -165,11 +165,11 @@ const productos = [
         descripcionLarga: "Este empaque está diseñado para conservar la frescura de alimentos preparados. Es resistente, seguro y perfecto para el transporte.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Bolsa_Plana/Plana1.png",
-            "../assets/images/productos/Bolsa_Plana/Plana2.png",
-            "../assets/images/productos/Bolsa_Plana/Plana3.png",
-            "../assets/images/productos/Bolsa_Plana/Plana4.png",
-            "../assets/images/productos/Bolsa_Plana/Plana5.png"
+            "assets/images/productos/Bolsa_Plana/Plana1.png",
+            "assets/images/productos/Bolsa_Plana/Plana2.png",
+            "assets/images/productos/Bolsa_Plana/Plana3.png",
+            "assets/images/productos/Bolsa_Plana/Plana4.png",
+            "assets/images/productos/Bolsa_Plana/Plana5.png"
         ],
         categorias: ["alimentos", "carnicos", "panaderia", "snacks"]
     },
@@ -180,8 +180,8 @@ const productos = [
         descripcionLarga: "Este empaque es perfecto para almacenar alimentos de manera práctica y reutilizable. Fabricado con materiales de alta calidad.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Bolsa_Selle_Ventral/SelleVentral1.png",
-            "../assets/images/productos/Bolsa_Selle_Ventral/SelleVentral2.png"
+            "assets/images/productos/Bolsa_Selle_Ventral/SelleVentral1.png",
+            "assets/images/productos/Bolsa_Selle_Ventral/SelleVentral2.png"
         ],
         categorias: ["alimentos", "cafe-chocolate"]
     },
@@ -192,11 +192,11 @@ const productos = [
         descripcionLarga: "El empaque Flex-Up es ideal para una amplia variedad de productos. Su diseño flexible lo hace perfecto para el almacenamiento y transporte.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Flex_Up/FlexUp1.png",
-            "../assets/images/productos/Flex_Up/FlexUp2.png",
-            "../assets/images/productos/Flex_Up/FlexUp3.png",
-            "../assets/images/productos/Flex_Up/FlexUp4.png",
-            "../assets/images/productos/Flex_Up/FlexUp5.png"
+            "assets/images/productos/Flex_Up/FlexUp1.png",
+            "assets/images/productos/Flex_Up/FlexUp2.png",
+            "assets/images/productos/Flex_Up/FlexUp3.png",
+            "assets/images/productos/Flex_Up/FlexUp4.png",
+            "assets/images/productos/Flex_Up/FlexUp5.png"
         ],
         categorias: ["alimentos", "cafe-chocolate", "panaderia"]
     },
@@ -207,11 +207,11 @@ const productos = [
         descripcionLarga: "El empaque Amipak está diseñado específicamente para carnes frescas, asegurando su conservación y frescura por más tiempo.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Amipak/Amipak1.png",
-            "../assets/images/productos/Amipak/Amipak2.png",
-            "../assets/images/productos/Amipak/Amipak3.png",
-            "../assets/images/productos/Amipak/Amipak4.png",
-            "../assets/images/productos/Amipak/Amipak5.png"
+            "assets/images/productos/Amipak/Amipak1.png",
+            "assets/images/productos/Amipak/Amipak2.png",
+            "assets/images/productos/Amipak/Amipak3.png",
+            "assets/images/productos/Amipak/Amipak4.png",
+            "assets/images/productos/Amipak/Amipak5.png"
         ],
         categorias: ["carnicos"]
     },
@@ -222,11 +222,11 @@ const productos = [
         descripcionLarga: "El Flow-Pack 4 Selles ofrece una protección superior para productos delicados, manteniéndolos frescos y seguros.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Flow_Pack_4_Selles/CuatroSelles1.png",
-            "../assets/images/productos/Flow_Pack_4_Selles/CuatroSelles2.png",
-            "../assets/images/productos/Flow_Pack_4_Selles/CuatroSelles3.png",
-            "../assets/images/productos/Flow_Pack_4_Selles/CuatroSelles4.png",
-            "../assets/images/productos/Flow_Pack_4_Selles/CuatroSelles5.png"
+            "assets/images/productos/Flow_Pack_4_Selles/CuatroSelles1.png",
+            "assets/images/productos/Flow_Pack_4_Selles/CuatroSelles2.png",
+            "assets/images/productos/Flow_Pack_4_Selles/CuatroSelles3.png",
+            "assets/images/productos/Flow_Pack_4_Selles/CuatroSelles4.png",
+            "assets/images/productos/Flow_Pack_4_Selles/CuatroSelles5.png"
         ],
         categorias: ["cafe-chocolate", "panaderia", "snacks"]
     },
@@ -237,10 +237,10 @@ const productos = [
         descripcionLarga: "Este empaque cuenta con una ventana transparente que permite visualizar el contenido, ideal para productos frescos y atractivos.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Flex_Up_con_Ventana/Ventana1.png",
-            "../assets/images/productos/Flex_Up_con_Ventana/Ventana2.png",
-            "../assets/images/productos/Flex_Up_con_Ventana/Ventana3.png",
-            "../assets/images/productos/Flex_Up_con_Ventana/Ventana4.webp"
+            "assets/images/productos/Flex_Up_con_Ventana/Ventana1.png",
+            "assets/images/productos/Flex_Up_con_Ventana/Ventana2.png",
+            "assets/images/productos/Flex_Up_con_Ventana/Ventana3.png",
+            "assets/images/productos/Flex_Up_con_Ventana/Ventana4.webp"
         ],
         categorias: ["mascotas", "panaderia", "snacks"]
     },
@@ -251,11 +251,11 @@ const productos = [
         descripcionLarga: "El empaque de colágeno es ideal para productos cárnicos, ofreciendo una excelente conservación y presentación.",
         precio: "$$$$$$",
         imagenes: [
-            "../assets/images/productos/Colageno/Colageno1.png",
-            "../assets/images/productos/Colageno/Colageno2.png",
-            "../assets/images/productos/Colageno/Colageno3.png",
-            "../assets/images/productos/Colageno/Colageno4.png",
-            "../assets/images/productos/Colageno/Colageno5.png"
+            "assets/images/productos/Colageno/Colageno1.png",
+            "assets/images/productos/Colageno/Colageno2.png",
+            "assets/images/productos/Colageno/Colageno3.png",
+            "assets/images/productos/Colageno/Colageno4.png",
+            "assets/images/productos/Colageno/Colageno5.png"
         ],
         categorias: ["carnicos"]
     }
