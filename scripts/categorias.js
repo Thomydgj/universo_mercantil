a0feb6238e26afe068ed2c90831620960a9e98ac
Thomas DG Jaimes <thomydgj@gmail.com>
2025-10-01 20:05:15 -0500

    rutas absolutas

--- scripts/categorias.js
+++ scripts/categorias.js
@@ -1,11 +1,11 @@
 // Datos de las categorías
 const categorias = [
-    { id: "alimentos", nombre: "Alimentos Preparados", imagen: "/assets/images/categorias/alimentos.png" },
-    { id: "cafe-chocolate", nombre: "Café y Chocolate", imagen: "/assets/images/categorias/cafe-chocolate.png" },
-    { id: "carnicos", nombre: "Cárnicos", imagen: "/assets/images/categorias/carnicos.png" },
-    { id: "mascotas", nombre: "Mascotas", imagen: "/assets/images/categorias/mascotas.png" },
-    { id: "panaderia", nombre: "Panadería y Repostería", imagen: "/assets/images/categorias/panaderia.png" },
-    { id: "snacks", nombre: "Snacks y Cereales", imagen: "/assets/images/categorias/snacks.png" }
+    { id: "alimentos", nombre: "Alimentos Preparados", imagen: "assets/images/categorias/alimentos.png" },
+    { id: "cafe-chocolate", nombre: "Café y Chocolate", imagen: "assets/images/categorias/cafe-chocolate.png" },
+    { id: "carnicos", nombre: "Cárnicos", imagen: "assets/images/categorias/carnicos.png" },
+    { id: "mascotas", nombre: "Mascotas", imagen: "assets/images/categorias/mascotas.png" },
+    { id: "panaderia", nombre: "Panadería y Repostería", imagen: "assets/images/categorias/panaderia.png" },
+    { id: "snacks", nombre: "Snacks y Cereales", imagen: "assets/images/categorias/snacks.png" }
 ];
 
 // Contenedor de categorías
